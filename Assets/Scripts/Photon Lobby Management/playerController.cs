bfd36297f6f712fbe7d569d93351bb363e5524cc
Funbam <bambamtheram@gmail.com>
2022-12-15 12:10:17 -0800

    Basic Schmoovement is in

--- Assets/Scripts/Photon Lobby Management/playerController.cs
+++ Assets/Scripts/Photon Lobby Management/playerController.cs
@@ -2,21 +2,82 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
     [SerializeField] Ability ability1;
-    
-    public PlayerManager myPM;
+
+    //Input Actions
+    private PlayerControllerInputAsset input;
+    public InputAction playerMove { get; private set; }
+    public InputAction playerJump { get; private set; }
+    public InputAction abilityOneAction { get; private set; }
+
+    //Standard move values
+    private Vector3 StartVelocity = Vector3.zero;
+    private Vector2 movementVector;
+    [Header("Movement Values")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float movementSmoothing;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float doubleJumpPower;
+    //Movement bools
+    private bool isGrounded;
+    private bool canDoubleJump;
+
+    //Grounded things
+    [Header("Grounded Check Items")]
+    [SerializeField] private Transform groundedCheckObject;
+    [SerializeField] private LayerMask groundLayer;
+
+    //Components
+    private PlayerManager myPM;
     private PhotonView myPV;
     private Rigidbody2D myRB;
-    // Start is called before the first frame update
+
     void Awake()
     {
         myPV = GetComponent<PhotonView>();
         myRB = GetComponent<Rigidbody2D>();
 
         myPM = PhotonView.Find((int) myPV.InstantiationData[0]).GetComponent<PlayerManager>();
+
+        input = new PlayerControllerInputAsset();
+        playerMove = input.Player.Move;
+        playerJump = input.Player.Jump;
+        abilityOneAction = input.Player.Ability1;
+
+        playerJump.started += jumpBehavior =>
+        {
+            if (!myPV.IsMine)
+            {
+                return;
+            }
+            //If grounded, jump normally
+            if (isGrounded)
+            {
+                myRB.AddForce(new Vector2(myRB.velocity.y, jumpPower * 25));
+            }
+
+            //If midair, check if can double jumo
+            else if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                myRB.velocity = new Vector2(myRB.velocity.x, 0);
+                myRB.AddForce(new Vector2(myRB.velocity.y, doubleJumpPower * 25));
+            }
+        };
+
+        abilityOneAction.started += ability1Behavior =>
+        {
+            if (!myPV.IsMine)
+            {
+                return;
+            }
+
+            ability1.Use();
+        };
     }
 
     // Update is called once per frame
@@ -26,19 +87,37 @@ public class playerController : MonoBehaviour
         if (!myPV.IsMine)
             return;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Manage grounded state
+        isGrounded = Physics2D.Linecast(transform.position, groundedCheckObject.position, groundLayer);
+        if (isGrounded)
         {
-            myRB.AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
+            canDoubleJump = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.B) & PhotonNetwork.IsMasterClient)
-        {
-            PhotonNetwork.LoadLevel("Lobby");
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            ability1.Use();
-        }
+        Movement();
     }
+
+    private void Movement()
+    {
+        //Grounded movement
+        movementVector = playerMove.ReadValue<Vector2>();
+        Vector3 VelocityChange = new Vector2(Time.fixedDeltaTime * moveSpeed * 10 * movementVector.x, myRB.velocity.y);
+        myRB.velocity = Vector3.SmoothDamp(myRB.velocity, VelocityChange, ref StartVelocity, movementSmoothing);
+
+    }
+
+    private void OnEnable()
+    {
+        playerMove.Enable();
+        playerJump.Enable();
+        abilityOneAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerMove.Disable();
+        playerJump.Disable();
+        abilityOneAction.Disable();
+    }
+
 }
