4503b9f1ac08e7584810610eeaec22a44255b7f9
Funbam <bambamtheram@gmail.com>
2023-01-28 23:27:04 -0800

    Kill requirement interface added and debugged

---
+++ Assets/Scripts/Photon Lobby Management/KillConditionItem.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+public class KillConditionItem : MonoBehaviourPunCallbacks
+{
+    public GameObject leftArrowButton;
+    public GameObject rightArrowButton;
+
+    ExitGames.Client.Photon.Hashtable playerProperties = new ExitGames.Client.Photon.Hashtable();
+    public TMP_Text kill_num_text;
+    public int minKills;
+    public int maxKills;
+    public int killIncrement;
+    private int currentKillNum;
+
+    private void Awake()
+    {
+        currentKillNum = minKills;
+        kill_num_text.text = currentKillNum.ToString();
+        ApplyChangesToAll();
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        ApplyChangesToAll();
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        leftArrowButton.SetActive(false);
+        rightArrowButton.SetActive(false);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        ApplyChangesToAll();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ApplyChangesToAll();
+    }
+
+    public void ApplyChangesToAll()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            leftArrowButton.SetActive(true);
+            rightArrowButton.SetActive(true);
+            //Kinda cursed but the master sets the kill win condition for everyone
+            foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
+            {
+                playerProperties["killsToWin"] = currentKillNum;
+                player.Value.SetCustomProperties(playerProperties);
+            }
+        }
+        else
+        {
+            leftArrowButton.SetActive(false);
+            rightArrowButton.SetActive(false);
+        }
+
+    }
+
+    public void OnClickLeft()
+    {
+        if (currentKillNum <= minKills)
+        {
+            currentKillNum = maxKills;
+        }
+        else
+        {
+            currentKillNum -= killIncrement;
+        }
+        kill_num_text.text = currentKillNum.ToString();
+        ApplyChangesToAll();
+    }
+
+    public void OnClickRight()
+    {
+        if (currentKillNum >= maxKills)
+        {
+            currentKillNum = minKills;
+        }
+        else
+        {
+            currentKillNum += killIncrement;
+        }
+        kill_num_text.text = currentKillNum.ToString();
+        ApplyChangesToAll();
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (targetPlayer == PhotonNetwork.LocalPlayer && targetPlayer.CustomProperties.ContainsKey("killsToWin"))
+        {
+            kill_num_text.text = targetPlayer.CustomProperties["killsToWin"].ToString();
+            currentKillNum = (int)targetPlayer.CustomProperties["killsToWin"];
+        }
+    }
+
+}
