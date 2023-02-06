26f27a219cb9e341e38ab48ddf23849eaacbc4c1
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-02-06 14:43:14 -0800

    Add trigger fix

--- Assets/Scripts/killbox.cs
+++ Assets/Scripts/killbox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class killbox : MonoBehaviour
 {
-    private void OnTriggerStayD(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
 
         // Get components
