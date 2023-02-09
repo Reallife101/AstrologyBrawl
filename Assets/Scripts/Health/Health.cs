582fa5a71bfd1ae0e9def61c3014621423b8cdea
Reallife101 <ryan.ngoon@gmail.com>
2023-02-09 09:56:51 -0800

    Add basic counter class

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -7,8 +7,10 @@ using Photon.Realtime;
 public abstract class Health : MonoBehaviourPunCallbacks, IDamageable
 {
     [SerializeField] const float MaxHealth = 100f;
+    [SerializeField] counter cntr;
 
     public float currentHealth = MaxHealth;
+    private bool counter = false;
 
     public PhotonView myPV;
 
@@ -34,23 +36,37 @@ public abstract class Health : MonoBehaviourPunCallbacks, IDamageable
     [PunRPC]
     public void RPC_TakeDamage(float damage, Vector2 launchVector, float hitStunValue, PhotonMessageInfo info)
     {
+        //Check if we countered
+        if (counter && cntr != null)
+        {
+            cntr.onCounter();
+            counter = false;
+            return;
+        }
+        
         myPV.RPC("HitStunned", myPV.Owner, hitStunValue);
         currentHealth -= damage;
 
         GetComponent<Rigidbody2D>().AddForce(launchVector, ForceMode2D.Impulse);
 
+        //if health below 0, die
         if (currentHealth <=0)
         {
             Die();
+
+            //if you are not yourself or nothing, give them a kill
             if (info.Sender != null && info.Sender != PhotonNetwork.LocalPlayer)
             {
                 PlayerManager.Find(info.Sender).GetKill();
             }
+            //if you have been damaged previously, give them a kill
             else if (lastPlayer != null)
             {
                 PlayerManager.Find(lastPlayer).GetKill();
             }
         }
+
+        //Update who damaged you last
         if (info.Sender != PhotonNetwork.LocalPlayer)
         {
             lastPlayer = info.Sender;
@@ -58,5 +74,16 @@ public abstract class Health : MonoBehaviourPunCallbacks, IDamageable
     }
 
     public abstract void Die();
+
+    //make getters and setters
+    public bool getCounter()
+    {
+        return counter;
+    }
+
+    public void setCounter(bool b)
+    {
+        counter = b;
+    }
 
 }
