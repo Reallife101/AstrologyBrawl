c8dc7fcb9aa6b6c6e978838051f8fa94872e598a
Eddy20015 <edwardj5@hotmail.com>
2023-02-12 17:37:04 -0800

    Update HealthHUDManager.cs

--- Assets/Scripts/Health/HealthHUDManager.cs
+++ Assets/Scripts/Health/HealthHUDManager.cs
@@ -9,25 +9,16 @@ using Photon.Pun;
 
 public class HealthHUDManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private string HealthItemString;
+    [SerializeField] private GameObject HealthItemPrefab;
     [SerializeField] private Transform HUDTransform;
 
     private List<HealthItem> HealthItems = new List<HealthItem>();
 
     public HealthItem AddHealthItem(string nickname, int actornum)
     {
-        HealthItem item = PhotonNetwork.Instantiate(HealthItemString, HUDTransform.position, HUDTransform.rotation).GetComponent<HealthItem>();
+        HealthItem item = Instantiate(HealthItemPrefab, HUDTransform).GetComponent<HealthItem>();
         item.Initialize(nickname, actornum);
         HealthItems.Add(item);
         return item;
-    }
-
-    public HealthItem FindMyHealthItem()
-    {
-        foreach(HealthItem item in HealthItems)
-        {
-            //if(//you, the player's, actor number is the same as one stored by a health thing, then this is your health!)
-        }
-        return null;
     }
 }
