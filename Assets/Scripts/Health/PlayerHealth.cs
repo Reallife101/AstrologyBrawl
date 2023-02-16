efb4f533886082c0e26915e72d75f3e68b4e911d
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-02-16 14:24:56 -0800

    add death feedback for kb and death

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -5,8 +5,14 @@ using Photon.Pun;
 
 public class PlayerHealth : Health
 {
+    [SerializeField] GameObject deathEffect;
     public override void Die()
     {
+        if (deathEffect != null)
+        {
+            PhotonNetwork.Instantiate(deathEffect.name, gameObject.transform.position, gameObject.transform.rotation, 0);
+        }
+
         PhotonView.Find((int)myPV.InstantiationData[0]).GetComponent<PlayerManager>().Die();
     }
 }
