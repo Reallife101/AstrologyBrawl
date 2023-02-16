776f6b2df0f14a07b7792f6e925e0dc1859e0254
Reallife101 <ryan.ngoon@gmail.com>
2023-02-16 12:31:42 -0800

    add better camera

--- Assets/Scripts/addPlayersToFollow.cs
+++ Assets/Scripts/addPlayersToFollow.cs
@@ -7,10 +7,30 @@ using Photon.Pun;
 public class addPlayersToFollow : MonoBehaviour
 {
     private CinemachineTargetGroup targetGroup;
+
+    [SerializeField]
+    CinemachineVirtualCamera VirtualCamera;
+
+    [SerializeField]
+    float DepthUpdateSpeed = 5f;
+    [SerializeField]
+    float minDepth = 5f;
+    [SerializeField]
+    float MaxDepth = 10f;
+    [SerializeField]
+    float minDistance = 5f;
+    [SerializeField]
+    float MaxDistance = 30f;
+
+    private float currDepth = 6f;
+
+    private GameObject[] people;
+
     // Start is called before the first frame update
     void Start()
     {
         targetGroup = GetComponent<CinemachineTargetGroup>();
+        people = GameObject.FindGameObjectsWithTag("Player");
     }
 
     public void removeMember(Transform t)
@@ -27,6 +47,7 @@ public class addPlayersToFollow : MonoBehaviour
 
     public void addAllMembers(GameObject[] players)
     {
+        people = players;
         foreach (GameObject go in players)
         {
             PhotonView pv = go.GetComponent<PhotonView>();
@@ -62,6 +83,61 @@ public class addPlayersToFollow : MonoBehaviour
     public void addMember(Transform t, float weight, float radius)
     {
         targetGroup.AddMember(t, weight, radius);
+    }
+
+    private void Update()
+    {
+        if (VirtualCamera != null)
+        {
+            calcDepth();
+            moveCamera();
+        }
+    }
+
+    private void calcDepth()
+    {
+        if (people != null)
+        {
+            
+            if (people.Length == 1)
+            {
+                currDepth = minDepth;
+                return;
+            }
+
+            float maxFound = 0f;
+            foreach (GameObject p in people)
+            {
+                if (p!=null)
+                {
+                    foreach (GameObject z in people)
+                    {
+                        if (z!=null)
+                        {
+                            float dist = Vector3.Distance(p.transform.position, z.transform.position);
+
+                            if (dist > maxFound)
+                            {
+                                maxFound = dist;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //calculate currDepth
+            float percent = Mathf.Clamp((maxFound - minDistance) / (MaxDistance - minDistance), 0, 1);
+            currDepth = minDepth + (MaxDepth - minDepth)* percent;
+
+        }
+    }
+
+    private void moveCamera()
+    {
+        if (VirtualCamera.m_Lens.OrthographicSize != currDepth)
+        {
+            VirtualCamera.m_Lens.OrthographicSize = Mathf.MoveTowards(VirtualCamera.m_Lens.OrthographicSize, currDepth, DepthUpdateSpeed * Time.deltaTime);
+        }
     }
 
 }
