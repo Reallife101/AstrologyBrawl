83b486bcd9448c9f6a235c0d9a29591f79a0c439
dfu7 <78887201+dfu7@users.noreply.github.com>
2023-02-21 14:46:35 -0800

    kill count under healthbar

--- Assets/Scripts/Photon Lobby Management/HealthItem.cs
+++ Assets/Scripts/Photon Lobby Management/HealthItem.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class HealthItem : MonoBehaviour
+public class HealthItem : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMPro.TMP_Text PlayerName;
     [SerializeField] private Slider HealthBar;
+    [SerializeField] private TMPro.TMP_Text KillText;
 
     private int OwnerActorNumber;
     private float CurrentHealth;
@@ -42,5 +45,16 @@ public class HealthItem : MonoBehaviour
             CurrentHealth = 0f;
         }
         HealthBar.value = CurrentHealth;
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey("kills"))
+        {
+            targetPlayer.CustomProperties.TryGetValue("kills", out object kills);
+            string KillString = (string)kills;
+
+            KillText.text = "Kills: " + KillString;
+        }
     }
 }
