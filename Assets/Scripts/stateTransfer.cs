669802f39d29bb86dd78281c831d357775d6d7f4
Reallife101 <ryan.ngoon@gmail.com>
2023-02-20 22:22:14 -0800

    add final mixup

--- Assets/Scripts/stateTransfer.cs
+++ Assets/Scripts/stateTransfer.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class stateTransfer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] StateManager stateManager;
+
+    public void startAnimation()
     {
-        
+        stateManager.StartCasting();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void endAnimation()
     {
-        
+        stateManager.EndCasting(.1f);
     }
 }
