99f2cd5f44cf5357e9449d615136040246920ed5
dfu7 <78887201+dfu7@users.noreply.github.com>
2023-02-25 15:59:44 -0800

    countdown manager 4

--- Assets/CountdownManager.cs
+++ Assets/CountdownManager.cs
@@ -6,21 +6,92 @@ using Photon.Realtime;
 
 public class CountdownManager : MonoBehaviourPunCallbacks
 {
+    ExitGames.Client.Photon.Hashtable ht;
+    double startTime;
+    bool startTimer;
+    double timerIncrementValue;
+    playerController[] playerControllerList;
+    int playerCount = 0;
+    bool playersFrozen = true;
 
-    private void Update()
+    [SerializeField] TMPro.TMP_Text countdownText;
+
+    private void FixedUpdate()
     {
-        if (GameObject.FindGameObjectsWithTag("Player").Length == PhotonNetwork.CurrentRoom.PlayerCount)
+        playerControllerList = FindObjectsOfType<playerController>();
+
+        if (playerControllerList.Length > playerCount)
         {
-            Debug.Log(GameObject.FindGameObjectsWithTag("Player").Length + " : " + PhotonNetwork.CurrentRoom.PlayerCount);
-            Countdown();
+            playerCount = playerControllerList.Length;
+            
+            foreach (playerController pc in playerControllerList)
+            {
+                pc.enabled = false;
+            }
+
+            if (!startTimer && playerControllerList.Length == PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                StartCountdownTimer();
+            }
         }
 
     }
 
-    void Countdown()
+    private void Update()
     {
-        // countdown
+        if (startTimer)
+        {
+            timerIncrementValue = PhotonNetwork.Time - startTime;
 
-        Destroy(gameObject);
+            if (timerIncrementValue >= 4)
+            {
+                Destroy(gameObject);
+            }
+            else if (timerIncrementValue >= 3)
+            {
+                countdownText.SetText("FIGHT!");
+
+                if (playersFrozen)
+                {
+                    foreach (playerController pc in playerControllerList)
+                    {
+                        pc.enabled = true;
+                    }
+
+                    playersFrozen = false;
+                }
+            }
+            else if (timerIncrementValue >= 2)
+            {
+                countdownText.SetText("1");
+            }
+            else if (timerIncrementValue >= 1)
+            {
+                countdownText.SetText("2");
+            }
+            else if (timerIncrementValue >= 0)
+            {
+                countdownText.SetText("3");
+            }
+        }
+    }
+
+
+
+    void StartCountdownTimer()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            ht = new ExitGames.Client.Photon.Hashtable();
+            startTime = PhotonNetwork.Time;
+            ht.Add("StartTime", startTime);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(ht);
+        }
+        else
+        {
+            startTime = double.Parse(PhotonNetwork.CurrentRoom.CustomProperties["StartTime"].ToString());
+        }
+
+        startTimer = true;
     }
 }
