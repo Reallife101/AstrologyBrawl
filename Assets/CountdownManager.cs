bde1fd9f66a3b10c3015d1d48aadb66e2d5d5cb9
dfu7 <78887201+dfu7@users.noreply.github.com>
2023-02-25 14:57:50 -0800

    Revert "countdown manager 3"
    This reverts commit 33c7c83fecacd7aa34459fc7659cdf92d4268c12.

--- Assets/CountdownManager.cs
+++ Assets/CountdownManager.cs
@@ -6,17 +6,12 @@ using Photon.Realtime;
 
 public class CountdownManager : MonoBehaviourPunCallbacks
 {
-    private void Start()
-    {
-        Application.targetFrameRate = 2;
-    }
 
     private void Update()
     {
         if (GameObject.FindGameObjectsWithTag("Player").Length == PhotonNetwork.CurrentRoom.PlayerCount)
         {
             Debug.Log(GameObject.FindGameObjectsWithTag("Player").Length + " : " + PhotonNetwork.CurrentRoom.PlayerCount);
-            Application.targetFrameRate = 30;
             Countdown();
         }
 
