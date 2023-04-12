a12fc120ca15c780a0706c854fae4aec26e247f2
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-04-11 21:59:14 -0700

    add initial tarot stuff

---
+++ Assets/pickUpCard.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class pickUpCard : MonoBehaviour
+{
+    private TarotCard t;
+    
+    void Start()
+    {
+        t = GetComponent<TarotCard>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<playerController>() != null)
+        {
+            Player p = collision.gameObject.GetComponent<PhotonView>().Owner;
+            t.Effect(p.ActorNumber);
+        }
+    }
+}
---
+++ Assets/tarotSpawnManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class tarotSpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> tarotCards;
+
+    private PhotonView PV;
+    private List<Transform> spawnPoints = new List<Transform>();
+
+    void Start()
+    {
+        GameObject spawnPointParent = GameObject.FindWithTag("SpawnPoints");
+        PV = GetComponent<PhotonView>();
+        foreach (Transform point in spawnPointParent.transform)
+        {
+            spawnPoints.Add(point);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.M))
+        {
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+            GameObject tarotCard = tarotCards[Random.Range(0, tarotCards.Count)];
+            PhotonNetwork.Instantiate(tarotCard.name, spawnPoint.position, Quaternion.identity, 0, new object[] { PV.ViewID });
+
+        }
+    }
+}
