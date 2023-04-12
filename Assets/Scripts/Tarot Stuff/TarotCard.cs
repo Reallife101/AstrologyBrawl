c9d438d02a2fa7e740574c7c6f48b1a49db52735
coleyoung02 <coleyoung741@gmail.com>
2023-04-12 12:46:02 -0700

    doTo function

--- Assets/Scripts/Tarot Stuff/TarotCard.cs
+++ Assets/Scripts/Tarot Stuff/TarotCard.cs
@@ -23,5 +23,35 @@ public abstract class TarotCard : MonoBehaviourPunCallbacks
         return cardName;
     }
 
+    //do not use for stuff like justice where everything has to be calculated at once before being applied
+    public void doTo(bool me, bool others, int actorNumber)
+    {
+        int aNum;
+        PhotonView[] controllers = FindObjectsOfType<PhotonView>();
+        if (me && others)
+        {
+            for (int i = 0; i < controllers.Length; ++i)
+            {
+                Effect(controllers[i].gameObject.GetComponent<PhotonView>().Owner.ActorNumber);
+            }
+        }
+        else if (me)
+        {
+            Effect(actorNumber);
+        }
+        else if (others)
+        {
+            aNum = -1;
+            for (int i = 0; i < controllers.Length; ++i)
+            {
+                aNum = controllers[i].gameObject.GetComponent<PhotonView>().Owner.ActorNumber;
+                if (aNum != actorNumber)
+                {
+                    Effect(aNum);
+                }
+            }
+        }
+    }
+
     public abstract void Effect(int actorNumber);
 }
