88b6676200d57216cd746d90ae2a96edc41b5690
Reallife101 <ryan.ngoon@gmail.com>
2023-05-01 09:47:25 -0700

    add health scripts

---
+++ Assets/Scripts/doGeminiDamage.cs
@@ -0,0 +1,12 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doGeminiDamage : doDamage
+{
+    public override bool extraDamageCheck()
+    {
+        return base.extraDamageCheck();
+    }
+}
--- Assets/Scripts/geminiToss.cs
+++ Assets/Scripts/geminiToss.cs
@@ -8,30 +8,41 @@ public class geminiToss : Ability
     [SerializeField] private GameObject littleTwin;
     [SerializeField] private Vector2 forceVector;
     [SerializeField] private float MaxDistance;
+    [SerializeField] private float returnSpeed;
+    [SerializeField] private float reattachDistance;
 
     private bool isAttached;
+    private bool isReturning;
     private Rigidbody2D rb;
-    private Vector3 startPosition;
+    private Vector3 playerPosition;
 
     public override void Use()
     {
+        //If little guy is returning, ability does nothing
+        if (isReturning)
+        {
+            return;
+        }
+
         if (isAttached)
         {
             littleTwin.transform.SetParent(null, true);
+            isAttached = false;
             toss();
 
         }
         else
         {
-            littleTwin.transform.SetParent(transform, true);
+            //littleTwin.transform.SetParent(transform, true);
+            bringBack();
         }
-        isAttached = !isAttached;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         isAttached = true;
+        isReturning = false;
         rb = littleTwin.GetComponent<Rigidbody2D>();
     }
 
@@ -39,12 +50,37 @@ public class geminiToss : Ability
     void Update()
     {
 
-        if (!isAttached && Vector2.Distance(littleTwin.transform.position, startPosition)>MaxDistance)
+        if (isReturning)
+        {
+            Vector3 diffVector = transform.position - littleTwin.transform.position;
+
+            //if we are close enough, stop returning
+            if (diffVector.magnitude < reattachDistance)
+            {
+                rb.velocity = Vector2.zero;
+                isReturning = false;
+                isAttached = true;
+                littleTwin.transform.SetParent(transform, true);
+
+            }
+            else
+            {
+                // Move little twin towards big twin
+                rb.velocity = (new Vector2(diffVector.x, diffVector.y)).normalized * returnSpeed;
+            }
+
+        }
+        else if (!isAttached && Vector2.Distance(littleTwin.transform.position, playerPosition)>MaxDistance)
         {
             rb.velocity = Vector2.zero;
 
         }
        
+    }
+
+    void bringBack()
+    {
+        isReturning = true;
     }
 
     void toss()
@@ -57,8 +93,7 @@ public class geminiToss : Ability
         {
             rb.velocity = forceVector;
         }
-
-        startPosition = transform.position;
+        playerPosition = transform.position;
     }
 
 
